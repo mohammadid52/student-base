4eec233010add0276b18af442aa8eb5fc05408b8
Mohammad <mohammadidehgamwala5@gmail.com>
2020-09-16 21:57:48 +0530

    Updates

---
+++ precache-manifest.fbed5cf806bb9f4ea1b0d11650e7c71d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed372d24782503642badb189faff25a",
+    "url": "/student-base/index.html"
+  },
+  {
+    "revision": "fe11d49cb6268e9a5902",
+    "url": "/student-base/static/css/main.f20955fb.chunk.css"
+  },
+  {
+    "revision": "fab3a34727d03e316e02",
+    "url": "/student-base/static/js/2.0ebfacab.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/student-base/static/js/2.0ebfacab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe11d49cb6268e9a5902",
+    "url": "/student-base/static/js/main.cc433baa.chunk.js"
+  },
+  {
+    "revision": "a33796d64b16754cbc64",
+    "url": "/student-base/static/js/runtime-main.14d413ad.js"
+  },
+  {
+    "revision": "09d49602f2a4927a138016ba337552b8",
+    "url": "/student-base/static/media/admin.09d49602.jpg"
+  },
+  {
+    "revision": "214ee42071647bf7d38799af1c3dfa5b",
+    "url": "/student-base/static/media/logo.214ee420.svg"
+  }
+]);
